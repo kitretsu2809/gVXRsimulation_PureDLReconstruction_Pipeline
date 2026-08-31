eeef20eba506e93e1af1a1252df632d3847182db
kitretsu <hrushikesh280904@gmail.com>
2026-08-31 16:13:43 +0530

    fix: place __future__ import at line 1

--- scripts/pure_dl/02_train_pure_dl.py
+++ scripts/pure_dl/02_train_pure_dl.py
@@ -1,7 +1,7 @@
+from __future__ import annotations
+
 import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-
-from __future__ import annotations
 
 import argparse
 import random
